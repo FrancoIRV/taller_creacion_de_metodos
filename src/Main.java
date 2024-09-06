a7ec73f25ac7af06eaa517afc9f540435924564f
Franco Ignacio Rivas Vidal <francorivasvidal01@gmail.com>
2024-09-06 08:58:34 -0400

    Primer avance del programa en el que se muestra el menu de opciones

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+        menu();
+    }
+    public static void menu(){
+        System.out.println("Bienvenido!");
+        System.out.println("Elige una opcion!");
+        System.out.println("1. Crear matriz");
+        System.out.println("2. Mostrar fila");
+        System.out.println("")
+    }
